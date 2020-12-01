6ac7c30ade6eadb1ddf025a78d8cef1c229c73c0
msenih <senih1999@gmail.com>
2020-12-01 15:08:27 -0600

    Added functional score system with UI.

---
+++ Assets/C#Scripts/GoalCollisions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalCollisions : MonoBehaviour
+{
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "Ball")
+        {
+            scoreBoard.ScoreHit();
+            Debug.Log("Point Scored!");
+        }
+    }
+}
---
+++ Assets/C#Scripts/ScoreBoard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    public int scorePerHit = 10;
+
+    public int score;
+    public Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void ScoreHit()
+    {
+        score = score + scorePerHit;
+        scoreText.text = score.ToString();
+        Debug.Log("scored " + score + " points");
+    }
+
+}
